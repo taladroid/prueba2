ef4494139436e407c2b86f985a471b729ba42f0c
taladroid <alebalshit@gmail.com>
2022-07-03 02:37:03 -0300

    pruebacommit

--- test/ar/edu/unlam/pb2/TestCases.java
+++ test/ar/edu/unlam/pb2/TestCases.java
@@ -8,18 +8,18 @@ public class TestCases {
 
 	@Test
 	public void queSePuedaCrearUnaTarjetaDeDebito() throws NumeroDeTarjetaInvalidoException{
-		// Preparación 
+		// Preparaciï¿½n 
 		final Long NUMERO_ESPERADO = 5446789813221201L;
 		final String TITULAR_ESPERADO = "CAMILA CIENFUEGOS";
 		final Integer CODIGO_DE_SEGURIDAD_ESPERADO = 123;
 		final String FECHA_DE_VENCIMIENTO_ESPERADO = "25/05/2022";
 		final double SALDO_ESPERADO = 10000.0;
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Pagadora tarjetaDeDebito = new TarjetaDeDebito(NUMERO_ESPERADO, TITULAR_ESPERADO, FECHA_DE_VENCIMIENTO_ESPERADO, CODIGO_DE_SEGURIDAD_ESPERADO);
 		tarjetaDeDebito.setSaldo(10000.0);
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(NUMERO_ESPERADO, tarjetaDeDebito.getNumero());
 		assertEquals(TITULAR_ESPERADO, tarjetaDeDebito.getTitular());
 		assertEquals(CODIGO_DE_SEGURIDAD_ESPERADO, tarjetaDeDebito.getCodigoDeSeguridad());
@@ -29,7 +29,7 @@ public class TestCases {
 	
 	@Test
 	public void queSePuedaCrearUnaTarjetaDeCredito() throws NumeroDeTarjetaInvalidoException{
-		// Preparación 
+		// Preparaciï¿½n 
 		final Long NUMERO_ESPERADO = 4246789813221201L;
 		final String TITULAR_ESPERADO = "SOFIA BARRIENTOS";
 		final Integer CODIGO_DE_SEGURIDAD_ESPERADO = 567;
@@ -37,10 +37,10 @@ public class TestCases {
 		final double LIMITE_COMPRA_EN_PESOS = 100000.0;
 		final double LIMITE_COMPRA_EN_DOLARES = 1000.0;
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Pagadora tarjetaDeCredito = new TarjetaDeCredito(NUMERO_ESPERADO, TITULAR_ESPERADO, FECHA_DE_VENCIMIENTO_ESPERADO, CODIGO_DE_SEGURIDAD_ESPERADO, LIMITE_COMPRA_EN_PESOS, LIMITE_DE_COMPRA_EN_DOLARES);
 
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(NUMERO_ESPERADO, tarjetaDeCredito.getNumero());
 		assertEquals(TITULAR_ESPERADO, tarjetaDeCredito.getTitular());
 		assertEquals(CODIGO_DE_SEGURIDAD_ESPERADO, tarjetaDeCredito.getCodigoDeSeguridad());
@@ -51,15 +51,15 @@ public class TestCases {
 	
 	@Test
 	public void queSePuedaCrearUnaCuentaBancaria() throws CBUInvalidoException{
-		// Preparación 
+		// Preparaciï¿½n 
 		final String CBU_ESPERADO = "01702046600000087865";
 		final String ENTIDAD_ESPERADA = "Santander";
 		final String TITULAR_ESPERADO = "Vicente De La Pradera";
 		
-		// Ejerución
+		// Ejeruciï¿½n
 		Transferible cuentaBancaria = new CuentaBancaria(CBU_ESPERADO, ENTIDAD_ESPERADA, TITULAR_ESPERADO);
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(CBU_ESPERADO, cuentaBancaria.getCbu());
 		assertEquals(ENTIDAD_ESPERADA, cuentaBancaria.getEntidad());
 		assertEquals(TITULAR_ESPERADO, cuentaBancaria.getTitular());
@@ -67,15 +67,15 @@ public class TestCases {
 	
 	@Test
 	public void queSePuedaCrearUnaCuentaVirtual() throws CVUInvalidoException{
-		// Preparación 
+		// Preparaciï¿½n 
 		final String CVU_ESPERADO = "46467898132212011234552";
 		final String ENTIDAD_ESPERADA = "Mercado Pago";
 		final String TITULAR_ESPERADO = "Leticia Bridgeton";
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Transferible cuentaVirtual = new CuentaVirtual(CVU_ESPERADO, ENTIDAD_ESPERADA, TITULAR_ESPERADO);
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(CVU_ESPERADO, cuentaVirtual.getCvu());
 		assertEquals(ENTIDAD_ESPERADA, cuentaVirtual.getEntidad());
 		assertEquals(TITULAR_ESPERADO, cuentaVirtual.getTitular());
@@ -83,103 +83,103 @@ public class TestCases {
 	
 	@Test
 	public void queSePuedaCrearUnComercio() throws CuitInvalidoException{
-		// Preparación 
+		// Preparaciï¿½n 
 		final String NOMBRE_ESPERADO = "El almacen de la esquina";
 		final Long CUIT_ESPERADO = 30215654124L;
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Comercio elAlmacen = new Comercio(CUIT_ESPERADO, NOMBRE_ESPERADO);
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(NOMBRE_ESPERADO, elAlmacen.getNomNbre());
 		assertEquals(NOMBRE_ESPERADO, elAlmacen.getCuit());
 	}
 	
 	@Test
 	public void queSePuedaCrearUnConsumidor() {
-		// Preparación 
+		// Preparaciï¿½n 
 		final String NOMBRE_ESPERADO = "Erika Romeo";
 		final Integer DNI_ESPERADO = 33458712;
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Consumidor nuevo = new Consumidor(DNI_ESPERADO, NOMBRE_ESPERADO);
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(NOMBRE_ESPERADO, elAlmacen.getNomNbre());
 		assertEquals(NOMBRE_ESPERADO, elAlmacen.getCuit());
 	}
 	
 	@Test
 	public void queSePuedanAgregarComerciosALaBilletera() throws CuitInvalidoException{
-		// Preparación 
+		// Preparaciï¿½n 
 		final Integer CANTIDAD_DE_COMERCIOS_ESPERADOS = 2;
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Billetera mataGalan = new Billetera("Ank");
 		
 		mataGalan.agregarComercio(new Comercio(30456213545L, "Supermercado"));
-		mataGalan.agregarComercio(new Comercio(30215654124L, "Panadería"));
-		mataGalan.agregarComercio(new Comercio(30215654124L, "Panadería"));
+		mataGalan.agregarComercio(new Comercio(30215654124L, "Panaderï¿½a"));
+		mataGalan.agregarComercio(new Comercio(30215654124L, "Panaderï¿½a"));
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(CANTIDAD_DE_COMERCIOS_ESPERADOS, billetera.getCantidadDeComercios());
 	}
 	
 	@Test
 	public void queSePuedanAgregarConsumidoresALaBilletera() {
-		// Preparación 
+		// Preparaciï¿½n 
 		final Integer CANTIDAD_DE_CONSUMIDORES_ESPERADOS = 2;
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Billetera mataGalan = new Billetera("Bna+");
 		
 		mataGalan.agregarConsumidor(Consumidor(27541231, "Luis Gomez");
 		mataGalan.agregarConsumidor(Consumidor(27541231, "Luis Gomez");
 		mataGalan.agregarConsumidor(Consumidor(36541231, "Sofia Molina");
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(CANTIDAD_DE_CONSUMIDORES_ESPERADOS, mataGalan.getCantidadDeConsumidores());
 	}
 	
 	@Test
 	public void queSePuedanAgregarDistintosMediosDePagoALaBilleteraDeUnConsumidor() throws NumeroDeTarjetaInvalidoException, CBUInvalidoException, CVUInvalidoException, NoCoincideTitularException{
-		// Preparación 
+		// Preparaciï¿½n 
 		final Integer CANTIDAD_DE_MEDIOS_EN_LA_BILLETERA_ESPERADOS = 4;
 		
-		// Ejecución
+		// Ejecuciï¿½n
 		Billetera mataGalan = new Billetera("MercadoPago");
 		mataGalan.agregarConsumidor(new Consumidor(27541231, "Luis Gomez");
 		
 		mataGalan.agregarMedioDePago(27541231, new TarjetaDeDebito(48332562153254623L, "Luis Gomez", "10/10/2026", 265));
 		mataGalan.agregarMedioDePago(27541231, new TarjetaDeDebito(48332562153254623L, "Luis Gomez", "10/10/2026", 312));
 		mataGalan.agregarMedioDePago(27541231, new TarjetaDeCredito(5423542385612354L, "Luis Gomez", "10/10/2026", 153));
-		mataGalan.agregarMedioDePago(27541231, new CuentaBancaria("01702046600000087865", "Nación", "Luis Gomez"));
+		mataGalan.agregarMedioDePago(27541231, new CuentaBancaria("01702046600000087865", "Naciï¿½n", "Luis Gomez"));
 		mataGalan.agregarMedioDePago(27541231, new CuentaVirtual("0000003100036521571806", "Mercado Pago", "Luis Gomez"));
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertEquals(CANTIDAD_DE_MEDIOS_EN_LA_BILLETERA_ESPERADOS, billetera.getCantidadDeMediosDePago(27541231));
 	}
 	
 	@Test
 	public void queSePuedaPagarConUnaTarjetaDeCredito() throws NumeroDeTarjetaInvalidoException, CBUInvalidoException, CVUInvalidoException, ExcedeLimiteDeCompraException{
-		// Preparación 
+		// Preparaciï¿½n 
 		Billetera mataGalan = new Billetera("Ank");
 		mataGalan.agregarConsumidor(Consumidor(27541231, "Luis Gomez");
 		mataGalan.agregarMedioDePago(27541231, new TarjetaDeCredito(48332562153254623L, "Luis Gomez", "10/10/2026", 265, 100000.0, 1000.0));
-		mataGalan.agregarComercio(new Comercio(30456213545L, "Panadería"));	
-		// Ejecución	
+		mataGalan.agregarComercio(new Comercio(30456213545L, "Panaderï¿½a"));	
+		// Ejecuciï¿½n	
 		Compra codigoQR = mataGalan.generarCodigoQR(30456213545L, 1000.0);
 		Boolean resultado = mataGalan.pagar(codigoQR, mataGalan.getConsumidor(27541231).getMedioPagador(48332562153254623L));
 		
-		// Verificación
+		// Verificaciï¿½n
 		assertTrue(resultado);
 	}
 	
 	@Test
 	public void queSePuedaTransferirDeUnaCuentaAOtra() throws NumeroDeTarjetaInvalidoException, CBUInvalidoException, CVUInvalidoException, SaldoInsuficienteException{
-		// Preparación 
-		Billetera mataGalan = new Billetera("Ualá");
-		Transferible cuentaOrigen = new CuentaBancaria("01702046600000087865", "Nación", "Luis Gomez");
+		// Preparaciï¿½n 
+		Billetera mataGalan = new Billetera("Ualï¿½");
+		Transferible cuentaOrigen = new CuentaBancaria("01702046600000087865", "Naciï¿½n", "Luis Gomez");
 		Consumidor consumidorOrigen = new Consumidor(27541231, "Luis Gomez");
 		mataGalan.agregarConsumidor(consumidorOrigen);
 		mataGalan.agregarMedioDePago(27541231, cuentaOrigen);
@@ -189,10 +189,10 @@ public class TestCases {
 		mataGalan.agregarConsumidor(consumidorDestino);
 		mataGalan.agregarMedioDePago(33896541, cuentaDestino);
 				
-		// Ejecución
+		// Ejecuciï¿½n
 		Boolean resultado = mataGalan.transferir(cuentaOrigen, cuentaDestino);
 				
-		// Verificación
+		// Verificaciï¿½n
 		assertTrue(resultado);
 	}
 	
@@ -209,6 +209,12 @@ public class TestCases {
 	@Test (expected=ExcedeLimiteDeCompraException.class)
 	public void queNoSePuedaComprarSiSeExcedeElLimiteDeCompraDeLaTarjetaDeCredito() {
 
+	}
+	
+	@Test
+	public void queSePuedaComitear () 
+	{
+		
 	}
 
 }
